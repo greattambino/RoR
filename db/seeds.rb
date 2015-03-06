dc20be5e1dc1df3d7de3aaf242ed19bdf91707cb
greattambino <marctambara@gmail.com>
2015-03-05 21:22:48 -0800

    Add password resets & email configuration

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,13 +16,13 @@ User.create!(name:  "Example User",
              activated_at: Time.zone.now)
 
 99.times do |n|
-  name = Faker::Name.name
+  name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
